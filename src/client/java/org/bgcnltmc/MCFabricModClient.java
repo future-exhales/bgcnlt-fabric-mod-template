ae5147552d6a1d041942572aed1719dfcfad398c
BGC Guest <bgcguest@BGCs-MacBook-Pro.local>
2026-03-12 12:59:28 -0700

    Initial commit

---
+++ src/client/java/org/bgcnltmc/MCFabricModClient.java
@@ -0,0 +1,10 @@
+package org.bgcnltmc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCFabricModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/org/bgcnltmc/ModItems.java
@@ -0,0 +1,4 @@
+package org.bgcnltmc;
+
+public class ModItems {
+}
